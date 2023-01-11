ce92ddf50721ec523de7ecbfcd6c992118faff1a
sallyzy0123 <sally_zy@protonmail.com>
2023-01-11 23:37:49 +0200

    add the keyextractor and try catch error

--- components/List.js
+++ components/List.js
@@ -5,10 +5,15 @@ import ListItem from './ListItem';
 const List = () => {
   const url = 'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
   const [mediaArray, setMediaArray] = useState([]);
+
   const loadMedia = async () => {
-    const response = await fetch(url);
-    const json = await response.json();
-    setMediaArray(json);
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      setMediaArray(json);
+    } catch (error) {
+      console.error('List, loadMedia', error);
+    }
   };
 
   useEffect(() => {
@@ -21,6 +26,7 @@ const List = () => {
     <>
       <FlatList
         data={mediaArray}
+        keyExtractor={(item, index) => index.toString()}
         renderItem={({item}) => <ListItem singleMedia={item} />}
       />
     </>
