94e558899121f52e04729664c13be6f86d74a66d
sallyzy0123 <sally_zy@protonmail.com>
2023-01-11 18:50:21 +0200

    development env setup ready

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+const App = () => {
+  //console.log('App starting!')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
