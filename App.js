97d407600c9353e4139f251fb41b732a6283d04c
sallyzy0123 <sally_zy@protonmail.com>
2023-01-12 09:59:14 +0200

    add the navigator between home and profile

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
 import {StatusBar} from 'expo-status-bar';
-import {Platform, StyleSheet, SafeAreaView} from 'react-native';
-import List from './components/List';
+import Navigator from './navigators/Navigator';
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <List />
+    <>
+      <Navigator></Navigator>
       <StatusBar style="auto" />
-    </SafeAreaView>
+    </>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: Platform.OS === 'android' ? 30 : 0,
-  },
-});
