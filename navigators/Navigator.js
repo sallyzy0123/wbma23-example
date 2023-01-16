03bcf6cb442957114d92a9811414625f729c3c12
sallyzy0123 <sally_zy@protonmail.com>
2023-01-16 10:00:39 +0200

    dummy login/logout buttons done

--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
@@ -6,6 +6,8 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import Home from '../views/Home';
 import Profile from '../views/Profile';
 import Single from '../views/Single';
+import Login from '../views/Login';
+import {MainContext} from '../contexts/MainContext';
 
 
 const Tab = createBottomTabNavigator();
@@ -33,11 +35,22 @@ const TabScreen = () => {
 };
 
 const StackScreen = () => {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name="Tabs" component={TabScreen} options={{headerShown:  false}}/>
-            <Stack.Screen name="Single" component={Single} />
-        </Stack.Navigator>
+  const {isLoggedIn} = useContext(MainContext);
+  return (
+    <Stack.Navigator>
+      {isLoggedIn ? (
+        <>
+          <Stack.Screen 
+            name="Tabs" 
+            component={TabScreen} 
+            options={{headerShown:  false}}
+          />
+          <Stack.Screen name="Single" component={Single} />
+        </>
+      ) : (
+        <Stack.Screen name="Login" component={Login}></Stack.Screen>
+      ) }   
+    </Stack.Navigator>
     )
 }
 
