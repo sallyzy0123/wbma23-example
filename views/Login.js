6a2259d57ade096c60842affdd851ce1495b0048
sallyzy0123 <sally_zy@protonmail.com>
2023-01-19 10:24:31 +0200

    login form done

--- views/Login.js
+++ views/Login.js
@@ -1,31 +1,21 @@
 import React, {useContext, useEffect} from 'react';
-import {StyleSheet,View,Text,Button} from 'react-native';
+import {StyleSheet,View,Text} from 'react-native';
 import PropTypes from 'prop-types';   
 import {MainContext} from '../contexts/MainContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {useAuthentication, useUser} from '../hooks/ApiHooks';
+import {useUser} from '../hooks/ApiHooks';
+import LoginForm from '../components/LoginForm';
 
 const Login = ({navigation}) => { 
   const {setIsLoggedIn} = useContext(MainContext);
-  const {postLogin} = useAuthentication();
   const {getUserByToken} = useUser();
 
-  const logIn = async () => {
-      console.log('Login button pressed');
-      const data = {username: 'yingzh', password: 'asdfg'};
-      try {
-        const loginResult = await postLogin(data);
-        console.log('login', loginResult);
-        await AsyncStorage.setItem('userToken', loginResult.token);
-        setIsLoggedIn(true);
-      } catch (error) {
-        console.warn('login', error);
-      }
-  };
 
   const checkToken = async () => {
     try {
        const userToken = await AsyncStorage.getItem('userToken');
+       // if no token available, do nothing
+       if (userToken === null) return;
        const userData = await getUserByToken(userToken);
        console.log('checkToken', userData);
        setIsLoggedIn(true);
@@ -41,7 +31,7 @@ const Login = ({navigation}) => {
   return (
     <View style={styles.container}>
       <Text>Login</Text>
-      <Button title="Sign in!" onPress={logIn}/>
+      <LoginForm />
     </View>
   );
 };
