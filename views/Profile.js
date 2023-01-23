499bf204105eda328d78b20124761deb3ae752f8
sallyzy0123 <sally_zy@protonmail.com>
2023-01-23 09:52:17 +0200

    avatar added

--- views/Profile.js
+++ views/Profile.js
@@ -1,13 +1,34 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import React, {useContext} from 'react';
-import {StyleSheet, SafeAreaView, Text, Button} from 'react-native';
+import React, {useContext, useEffect, useState} from 'react';
+import {StyleSheet, SafeAreaView, Text, Button, Image} from 'react-native';
 import { MainContext} from '../contexts/MainContext';
+import {useTag} from '../hooks/ApiHooks';
+import {uploadsUrl} from '../utils/variables';
+import {baseUrl} from '../utils/variables';
 
 const Profile = () => {
+  const {getFilesByTag} = useTag();
   const {setIsLoggedIn, user, setUser} = useContext(MainContext);
+  const [avatar, setAvatar] = useState('');
+
+  const loadAvatar = async () => {
+    try {
+      const avatarArray = await getFilesByTag('avatar_' + user.user_id);
+      setAvatar(avatarArray.pop().filename);
+    } catch (error) {
+      console.log('user avatar fetch failed', error.message);
+    }
+
+  };
+
+  useEffect(() => {
+    loadAvatar();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>Profile</Text>
+      <Image style={styles.image} source={{uri: uploadsUrl + avatar}} />
       <Text>username: {user.username}</Text>
       <Text>Email:{user.email}</Text>
       <Text>Full name: {user.full_name}</Text>
@@ -32,6 +53,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     paddingTop: 40,
+  },
+  image: {
+    width: 200,
+    height: 300,
   },
 });
 
